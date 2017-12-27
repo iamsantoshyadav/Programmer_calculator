8a78255e478946dcb8c291893d3be654bf15c0c1
Santosh Yadav <yadav7866@gmail.com>
2017-12-27 20:24:54 +0530

    Decimal to octal and vice versa updated
    Two new functions are created which perform decimal to octal and octal to decimal conversion

--- programer's_calculator.py
+++ programer's_calculator.py
@@ -9,12 +9,17 @@ def dec_to_binary(ip):
             rem=dec%2
             binno.append(rem)
             dec=int(dec/2)
-            #List(reversed(list_name) is use for reverse the list)
-        print("Binary no for ",ip,"is",list(reversed(binno)))
+                
     except :
         #Error message for the users
         print("You did not entered a valid value Please try again")
-
+    #List(reversed(list_name) is use for reverse the list)
+    strdata=""
+    binno = list(reversed(binno))  
+    for no in range(len(binno)):
+        string=str(binno[no])
+        strdata=strdata+string
+    print("Binary no for ",ip,"is",strdata)
 #this program is for performing binary to decimal conversion
 def binary_to_dec(ip):
     deci=0
@@ -28,11 +33,12 @@ def binary_to_dec(ip):
             #Storing all the single bit into a single variable or list
             data.append(integers)
         i=0
-        deci=0
+        
         #All the magics begin here
         for nos in range(len(data)-1,-1,-1):
             if data[nos]not in {0,1}:
                 print("Enter a valid binary no")
+                deci=0
                 break
             else :
                 i=(len(data)-1)-nos
@@ -46,6 +52,7 @@ def binary_to_dec(ip):
         print("You did not entered a valid value PLEASE ENTER BINBARY NO CAREFULLY")
         
         
+
 # THis function is for converting decimal no into octal no
 def dec_to_octal(ip):
     data=[]
@@ -55,7 +62,43 @@ def dec_to_octal(ip):
             rem=dec%8
             data.append(rem)
             dec=int(dec/8)
+        data=list(reversed(data))
+        strdata=""
+        for no in range(len(data)):
+            string=str(data[no])
+            strdata=strdata+string
+        print("Octal conversion of decimal no ",ip,"is",strdata)
     except :
         print("Enter a valid decimal value carefully");
-    print("Octal conversion of decimal no ",ip,"is",list(reversed(data)))
+   
+
+
+def octal_to_decimal(ip):
+    data=[]
+    octal=0
+    try :
+        ip=str(ip)
+        length=len(ip)
+        for nos in range(length):
+            data1=int(ip[nos])
+            data.append(data1)
+        for no in range(length-1,-1,-1):
+            if data[no] not in {0,1,2,3,4,5,6,7}:
+                print("Entered no. is not an octal no Please try again")
+                octal=0
+                break
+            else :
+                i=(length-1)-no
+                octal=octal+(data[no]*(8**i))
+        if octal>0 :
+            print("Decimal equivelent no of ",ip,"is : ",octal)
+        else :
+            print("Operation can not be purform for no ",ip)
+            
+    except :
+        print("You have not entered a valid value Please enter a valid octal no")
+        
 #%%
+stri=""+"yadav"
+stri=stri+"name"
+print(stri)
